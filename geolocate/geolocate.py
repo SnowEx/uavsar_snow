1edd877ba7b86b61f50a0b2a096acf6dabd920bd
Zach Keskinen <zachkeskinen@gmail.com>
2022-06-22 11:34:01 -0600

    fixed pathing errors

--- geolocate/geolocate.py
+++ geolocate/geolocate.py
@@ -8,9 +8,55 @@ import rasterio as rio
 from osgeo import gdal
 from uavsar_pytools.convert.tiff_conversion import read_annotation
 
-import sys
-sys.path.append('/Users/zachkeskinen/Documents/uavsar_snow/functions')
-from geocoding import geocodeUsingGdalWarp
+def geocodeUsingGdalWarp(infile, latfile, lonfile, outfile,
+                         insrs=4326, outsrs=None,
+                         spacing=None, fmt='GTiff', bounds=None,
+                         method='near'):
+    '''
+    Geocode a swath file using corresponding lat, lon files
+    '''
+    sourcexmltmpl = '''    <SimpleSource>
+      <SourceFilename>{0}</SourceFilename>
+      <SourceBand>{1}</SourceBand>
+    </SimpleSource>'''
+    
+    driver = gdal.GetDriverByName('VRT')
+    tempvrtname = 'temp_ele.vrt'
+    inds = gdal.OpenShared(infile, gdal.GA_ReadOnly)
+    
+    tempds = driver.Create(tempvrtname, inds.RasterXSize, inds.RasterYSize, 0)
+    
+    for ii in range(inds.RasterCount):
+        band = inds.GetRasterBand(1)
+        tempds.AddBand(band.DataType)
+        tempds.GetRasterBand(ii+1).SetMetadata({'source_0': sourcexmltmpl.format(infile, ii+1)}, 'vrt_sources')
+  
+    sref = osr.SpatialReference()
+    sref.ImportFromEPSG(insrs)
+    srswkt = sref.ExportToWkt()
+    tempds.SetMetadata({'SRS' : srswkt,
+                        'X_DATASET': lonfile,
+                        'X_BAND' : '1',
+                        'Y_DATASET': latfile,
+                        'Y_BAND' : '1',
+                        'PIXEL_OFFSET' : '0',
+                        'LINE_OFFSET' : '0',
+                        'PIXEL_STEP' : '1',
+                        'LINE_STEP' : '1'}, 
+                        'GEOLOCATION')
+    
+    band = None
+    tempds = None 
+    inds = None
+    
+    if spacing is None:
+        spacing = [None, None]
+    warpOptions = gdal.WarpOptions(format=fmt,
+                                   xRes=spacing[0], yRes=spacing[0],
+                                   dstSRS=outsrs, outputBounds = bounds, 
+                                   resampleAlg=method, geoloc=True)
+    gdal.Warp(outfile, tempvrtname, options=warpOptions)
+    os.remove('temp_ele.vrt')
 
 def geolocate_uavsar(in_fp, ann_fp, out_dir, llh_fp):
     """
