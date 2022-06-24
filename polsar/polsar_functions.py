d81b33fe8c46b9a3f52312c44e7af18c445d7703
Ross Palomaki <ross.palomaki@gmail.com>
2022-06-24 11:15:26 -0600

    add polsar_functions.py

---
+++ polsar/polsar_functions.py
@@ -0,0 +1,56 @@
+"""
+https://dges.carleton.ca/courses/IntroSAR/Winter2019/SECTION%203%20-%20Carleton%20SAR%20Training%20-%20SAR%20Polarimetry%20%20-%20Final.pdf
+"""
+
+def ingest_polsar():
+    """
+
+    """
+
+
+def calc_C3():
+    """
+
+    """
+
+
+def C3_to_T3():
+    """
+    https://github.com/EO-College/polsarpro/blob/master/Soft/src/lib/util_convert.c
+    use more readable version
+    """
+
+def T3_to_alpha1():
+    """
+    
+    """
+
+
+def T3_to_mean_alpha():
+    """
+    
+    """
+
+
+def uavsar_alpha1():
+    """
+    
+    """
+
+
+def uavsar_meanalpha():
+    """
+    
+    """
+
+
+def uavsar_H():
+    """"
+    entropy
+    """
+
+
+def uavsar_A():
+    """
+    anisotropy
+    """
