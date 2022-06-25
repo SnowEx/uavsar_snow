8d3d0c841a3f6a06b5d2d5d6d169b1cd3a95892b
ZachKeskinen <zachkeskinen@gmail.com>
2022-06-24 21:15:40 -0600

    testing dask

--- polsar/function_test.py
+++ polsar/function_test.py
@@ -5,7 +5,7 @@ st = time()
 
 in_dir = '/home/zacharykeskinen/Documents/uavsar_snow/polsar_imgs/uticam_21003_21004_002_210120_L090_CX_01_grd'
 out_dir = '/home/zacharykeskinen/Documents/uavsar_snow/polsar_imgs/testing'
-out = H_A_alpha_decomp(in_dir, out_dir)
+out = H_A_alpha_decomp(in_dir, out_dir, parralel =True)
 
 et = time()
 print(f'Time: {(et-st)/60} minutes')
